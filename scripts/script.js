bdec4b333aca8901bcb4e7c5aa3b4e86f373f96a
Vladimir Stebikhov <stebikhov@yandex.ru>
2025-05-04 18:40:02 +0300

    Fix comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 /* КОНФИГ */
 const preloaderWaitindTime = 1200;
 const cardsOnPage = 5;
-const BASE_URL = 'https://v-content.practicum-team.ru';
+const BASE_URL = 'http://localhost:3000';
 const endpoint = `${BASE_URL}/api/videos?pagination[pageSize]=${cardsOnPage}&`;
 
 /* ЭЛЕМЕНТЫ СТРАНИЦЫ */
